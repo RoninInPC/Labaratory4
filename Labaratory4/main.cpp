38ee24f0f2f549e1ab99c9386e625954df4a8e3f
RoninInPC <62109908+RoninInPC@users.noreply.github.com>
2021-11-21 14:20:12 +0500

    таблицы

--- Labaratory4/main.cpp
+++ Labaratory4/main.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include <iomanip>
 #include<math.h>//подключение библиотек 
 using namespace std;//строчка для того чтобы убрать обращение к полю std::, например std::cin
 struct CountAnsSum{ //структура для вывода результата: суммы ряда и количества членов в ряду
@@ -27,22 +28,54 @@ CountAnsSum LnPlusOne(double X, double eps) {
 int main() {
 	double X_start, X_end, delta_X,eps;
 	cin >> X_start>>X_end>>delta_X>>eps;//ввод чисeл
+	cout.width(15);
+	cout << "X";
+	cout.width(15);
+	cout << "MyFunction(X)";
+	cout.width(15);
+	cout << "Function(X)";
+	cout.width(15);
+	cout << "U" << endl;
 	for (double i = X_start; i <= X_end; i += delta_X) {
 		CountAnsSum ans1 = LnPlusOne(i, eps); //вызов написанной функции
 		double ans2 = log(i + 1);//вызов функции из math.h
 		double nevyska = sqrt(abs(ans1.SumRow * ans1.SumRow - ans2 * ans2)); //невязка или nevyska
-		cout << "Ans My Ln " << ans1.SumRow << " Ans math Ln " << ans2 << " Nevyska " << nevyska << " Count of Row "
-			<< ans1.CountInRow << endl; //вывод всех результатов, нужных для лабы
+		cout.width(15);
+		cout << i;
+		cout.width(15);
+		cout << ans1.SumRow;
+		cout.width(15);
+		cout << ans2;
+		cout.width(15);
+		cout << nevyska<<endl; //вывод всех результатов, нужных для лабы
 	}
 	cout << endl;
 	double X;
 	cin >> X;
+	cout.width(15);
+	cout << "eps";
+	cout.width(15);
+	cout << "X";
+	cout.width(15);
+	cout << "Myfunction(X)";
+	cout.width(15);
+	cout << "Function(X)";
+	cout.width(15);
+	cout << "U" << endl;
 	for (double eps = 0.1; eps >= 0.0000001; eps /= 10) {
 		CountAnsSum ans1 = LnPlusOne(X, eps); //вызов написанной функции
 		double ans2 = log(X + 1);//вызов функции из math.h
 		double nevyska = sqrt(abs(ans1.SumRow * ans1.SumRow - ans2 * ans2)); //невязка или nevyska
-		cout << "Ans My Ln " << ans1.SumRow << " Ans math Ln " << ans2 << " Nevyska " << nevyska << " Count of Row "
-			<< ans1.CountInRow << endl; //вывод всех результатов, нужных для лабы
+		cout.width(15);
+		cout << eps;
+		cout.width(15);
+		cout << X;
+		cout.width(15);
+		cout << ans1.SumRow;
+		cout.width(15);
+		cout << ans2;
+		cout.width(15); 
+		cout << nevyska << endl; //вывод всех результатов, нужных для лабы
 	}
 
 	return 0;
